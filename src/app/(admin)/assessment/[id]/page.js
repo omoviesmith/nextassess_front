7d73a4a3a3641b90d5acd4df9dbafa95c1a0ec13
shoaibmehmood243 <shoaibmehmood065@gmail.com>
2024-08-21 14:11:22 +0500

    fixed assessment issue

--- src/app/(admin)/assessment/[id]/page.js
+++ src/app/(admin)/assessment/[id]/page.js
@@ -5,16 +5,13 @@ const EditAssessment = dynamic(() => import('@/components/Assessment/Edit'),{
 
 const fetchData = async (shardId,id) => {
     try {
-      console.log(`https://cqzb53kpam.ap-southeast-2.awsapprunner.com/api/assessments/${shardId}/${id}`);
       const res = await fetch(`https://cqzb53kpam.ap-southeast-2.awsapprunner.com/api/assessments/${shardId}/${id}`, {
         cache: 'no-store', // to ensure fresh data on every request
       });
-      console.log(res);
       if (!res.ok) {
         throw new Error('Failed to fetch data');
       }
       const data = await res.json();
-      console.log(data);
       return data;
     } catch (error) {
       return null;
--- src/components/Assessment/Table.js
+++ src/components/Assessment/Table.js
@@ -79,8 +79,8 @@ export default function Table({ data = [] }) {
                                         </td>
                                         <td className="border-b border-slate-300 bg-white p-3">
                                             <div className="flex gap-2 justify-center">
-                                                <DeleteAssessment shardId={assessment.shardId} id={assessment.id} />
-                                                <Link href={`/assessment/${assessment.id}?shardId=${assessment.shardId}`}>
+                                                <DeleteAssessment shardId={assessment.shardId} id={assessment.assessmentId} />
+                                                <Link href={`/assessment/${assessment.assessmentId}?shardId=${assessment.shardId}`}>
                                                     <svg className="cursor-pointer" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
                                                         <g clipPath="url(#clip0_16_14087)">
                                                             <path d="M14.06 9.02L14.98 9.94L5.92 19H5V18.08L14.06 9.02ZM17.66 3C17.41 3 17.15 3.1 16.96 3.29L15.13 5.12L18.88 8.87L20.71 7.04C21.1 6.65 21.1 6.02 20.71 5.63L18.37 3.29C18.17 3.09 17.92 3 17.66 3ZM14.06 6.19L3 17.25V21H6.75L17.81 9.94L14.06 6.19Z" fill="#323232" />
