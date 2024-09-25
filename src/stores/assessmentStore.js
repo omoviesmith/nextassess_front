bf3dfe835cfca4aa591d9cac411d0b3e1e023c2f
Smith <omoviesmith@gmail.com>
2024-09-25 23:13:35 +0100

    marking rubric

--- src/stores/assessmentStore.js
+++ src/stores/assessmentStore.js
@@ -1,3 +1,21 @@
+// import { create } from 'zustand';
+
+// const useAssessmentStore = create((set) => ({
+//   assessments: [],
+//   pagination: {
+//     total_items: 0,
+//     items_per_page: 10,
+//     current_page: 1,
+//   },
+//   setAssessments: (data) => set({ 
+//     assessments: data.assessments || [], 
+//     pagination: data.pagination || {} 
+//   }),
+//   setPagination: (paginationData) => set({ pagination: paginationData }),
+// }));
+
+// export default useAssessmentStore;
+
 import { create } from 'zustand';
 
 const useAssessmentStore = create((set) => ({
@@ -7,11 +25,13 @@ const useAssessmentStore = create((set) => ({
     items_per_page: 10,
     current_page: 1,
   },
-  setAssessments: (data) => set({ 
-    assessments: data.assessments || [], 
-    pagination: data.pagination || {} 
-  }),
-  setPagination: (paginationData) => set({ pagination: paginationData }),
+  setAssessments: (data) =>
+    set({
+      assessments: data.assessments || [],
+      pagination: data.pagination || {},
+    }),
+  setPagination: (paginationData) =>
+    set({ pagination: paginationData }),
 }));
 
 export default useAssessmentStore;
