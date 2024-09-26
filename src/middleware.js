d99db8f4cefd211664345110f8aaa5b9a6acca17
Smith <omoviesmith@gmail.com>
2024-09-26 12:50:53 +0100

    state management

--- src/middleware.js
+++ src/middleware.js
@@ -1,7 +1,43 @@
+// import { NextResponse } from 'next/server';
+
+// export function middleware(request) {
+//     const user = request.cookies.get('user');
+
+//     if (!user && request.nextUrl.pathname !== '/') {
+//         return NextResponse.redirect(new URL('/', request.url));
+//     }
+
+//     if (user && request.nextUrl.pathname === '/') {
+//         return NextResponse.redirect(new URL('/assessments', request.url));
+//     }
+// }
+
+// export const config = {
+//     matcher: [
+//         '/admin/:path*',
+//         '/assessments',
+//         '/assessment/:path*',
+//         '/courses',
+//         '/support',
+//         '/subscription',
+//         '/users',
+//         '/account',
+//         '/'
+//     ],
+// };
+
+
 import { NextResponse } from 'next/server';
 
 export function middleware(request) {
-    const user = request.cookies.get('user');
+    const userCookie = request.cookies.get('user');
+
+    let user = null;
+    try {
+        user = userCookie ? JSON.parse(userCookie) : null;
+    } catch (error) {
+        console.error('Failed to parse user cookie:', error);
+    }
 
     if (!user && request.nextUrl.pathname !== '/') {
         return NextResponse.redirect(new URL('/', request.url));
